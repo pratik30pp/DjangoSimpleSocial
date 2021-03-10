92fee1753a2ae893b03339c66ffca9db592132d3
pratik30pp <pratik3010pp@gmail.com>
2021-03-10 22:08:35 +0530

    before group and post migrations. Till part 8 udemy

--- simplesocial/urls.py
+++ simplesocial/urls.py
@@ -13,15 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf.urls import url, include
-from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.HomePage.as_view(), name='home'),
+    url(r"^admin/", admin.site.urls),
+    url(r"^$", views.HomePage.as_view(), name="home"),
     
-    url(r'accounts/',include('accounts.urls',namespace='accounts')),
-    url(r'accounts/', include('django.contrib.auth.urls')),
+    url(r'^accounts/', include('accounts.urls',namespace='accounts')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^test/$', views.TestPage.as_view(), name='test'),
+    url(r'^thanks/$', views.ThanksPage.as_view(), name='thanks'),
 ]
